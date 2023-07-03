4f5d21bbff9ff6b20526243f3941901e4f1046de
guri1423 <104896049+guri1423@users.noreply.github.com>
2023-07-03 06:17:55 +0530

    some files added

---
+++ lib/services/api_services.dart
@@ -0,0 +1,19 @@
+
+
+class RemoteApi {
+
+/*  Future<LoginResponseModel?> userLogin(LoginRequestModal model) async {
+    try {
+      final response =
+      await http.post(Uri.parse(Urls.login), body: model.toJson());
+
+      debugPrint(response.body);
+      var jsonResponse = json.decode(response.body);
+      debugPrint('json respONSE ${jsonResponse}');
+      return loginResponseModelFromJson(response.body);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+    return null;
+  }*/
+}
---
+++ lib/services/storage_services.dart
@@ -0,0 +1,65 @@
+
+
+import '../core/strings.dart';
+
+class StorageServices{
+
+
+/*  final storage =  const FlutterSecureStorage();
+  setAddress(String address)async{
+    await storage.write(key: Strings.address, value: address);
+  }
+  setEmail(String email)async{
+    await storage.write(key: Strings.emailKey, value: email);
+  }
+
+  setPassword(String password)async{
+    await storage.write(key: Strings.passwordKey, value: password);
+  }
+  removeEmailAndPass()async{
+    await storage.delete(key: Strings.emailKey);
+    await storage.delete(key: Strings.passwordKey);
+  }
+  setUserLoggedIn(String val)async{
+    await storage.write(key: Strings.loggedKey, value: val);
+  }
+
+  Future<String?> getEmail()async{
+    return await storage.read(key: Strings.emailKey);
+
+  }
+  Future<String?> getAddress()async{
+    return await storage.read(key: Strings.address);
+
+  }
+  Future<String?> getPassword()async{
+    return await storage.read(key: Strings.passwordKey);
+
+  }
+
+  setUserId(String id)async{
+    await storage.write(key: Strings.userIdKey, value: id);
+  }
+
+  Future<String?>getUserId()async{
+    return await storage.read(key: Strings.userIdKey);
+  }
+
+  setId(String id)async{
+    await storage.write(key: Strings.IdKey, value: id);
+  }
+
+  Future<String?>getId()async{
+    return await storage.read(key: Strings.IdKey);
+  }
+
+  Future<String?>getLoggedInStatus()async{
+    return await storage.read(key: Strings.loggedKey);
+  }
+
+
+  clearStorage()async{
+    await storage.deleteAll();
+  }*/
+
+}
